083b05b885a088bdaa656bfcfaa46f2fe1086eb8
mathisarends <mathisarends27@gmail.com>
2025-06-02 21:59:39 +0200

    Setup basic cli for setup

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="notionary",
-    version="0.2.10",
+    version="0.2.11",
     packages=find_namespace_packages(include=["notionary*"]),
     install_requires=[
         "httpx>=0.28.0",
         "python-dotenv>=1.1.0",
         "pydantic>=2.11.4",
         "posthog>=3.0.0",
+        "click>=8.0.0", 
     ],
+    entry_points={
+        'console_scripts': [
+            'notionary=notionary.cli.main:main',
+        ],
+    },
     author="Mathis Arends",
     author_email="mathisarends27@gmail.com",
     description="A toolkit to convert between Markdown and Notion blocks",
