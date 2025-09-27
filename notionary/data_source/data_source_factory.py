19c12a36ddc5f399082ffffe3a9ac45a94ec9e4d
mathisarends <mathisarends27@gmail.com>
2025-09-27 15:52:29 +0200

    Small tweaks to data_source_facotry for improved algorithm

--- notionary/data_source/data_source_factory.py
+++ notionary/data_source/data_source_factory.py
@@ -35,22 +35,22 @@ class DataSourceFactory(BaseFactory, ParentExtractMixin):
 
     async def load_from_title(self, data_source_title: str, min_similarity: float = 0.6) -> NotionDataSource:
         search_response = await self._data_source_client.search_data_sources(data_source_title)
-        potential_data_sources = await self._extract_potential_data_sources_from_search_result(search_response)
+        data_source_candidates = await self._extract_potential_data_sources_from_search_result(search_response)
 
         best_match = find_best_match(
             query=data_source_title,
-            items=potential_data_sources,
+            items=data_source_candidates,
             text_extractor=lambda data_source: data_source.title,
             min_similarity=min_similarity,
         )
 
         if not best_match:
-            available_titles = [result.title for result in potential_data_sources[:5]]
+            available_titles = [result.title for result in data_source_candidates[:5]]
             raise ValueError(
                 f"No sufficiently similar data source found for '{data_source_title}'. Available: {available_titles}"
             )
 
-        return await self.load_from_id(best_match.item.id)
+        return best_match.item
 
     async def _extract_potential_data_sources_from_search_result(
         self, search_response: DataSourceSearchResponse
@@ -64,9 +64,11 @@ class DataSourceFactory(BaseFactory, ParentExtractMixin):
     async def _create_from_response(self, data_source_dto: DataSourceDto) -> NotionDataSource:
         entity_kwargs = self._create_common_entity_kwargs(data_source_dto)
 
-        title = await self._extract_title(data_source_dto)
-        description = await self._extract_description(data_source_dto)
-        parent_database = await self._extract_parent_database(data_source_dto)
+        title, description, parent_database = await asyncio.gather(
+            self._extract_title(data_source_dto),
+            self._extract_description(data_source_dto),
+            self._extract_parent_database(data_source_dto),
+        )
 
         entity_kwargs.update(
             {
