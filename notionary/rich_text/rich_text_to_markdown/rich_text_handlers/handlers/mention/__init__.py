32943056e3d7fd5cc9541803c72b9d24ba9691f5
mathisarends <mathisarends27@gmail.com>
2025-11-06 06:16:43 +0100

    Use du for mention types and fix handler tests

--- notionary/rich_text/rich_text_to_markdown/rich_text_handlers/handlers/mention/__init__.py
+++ notionary/rich_text/rich_text_to_markdown/rich_text_handlers/handlers/mention/__init__.py
@@ -1,11 +1,11 @@
 from .factory import (
     create_mention_handler_registry,
 )
+from .handler import MentionRichTextHandler
 from .handlers.base import MentionHandler
 from .registry import (
     MentionHandlerRegistry,
 )
-from .rich_text_handler import MentionRichTextHandler
 
 __all__ = [
     "MentionHandler",
